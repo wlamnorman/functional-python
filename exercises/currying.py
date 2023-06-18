52db28657b82a4268d31f4cf9f036fac818b6119
William Norman <william@abios.se>
2023-06-18 21:26:24 +0200

    currying

---
+++ exercises/currying.py
@@ -0,0 +1,15 @@
+def curry_add(x):
+    def curry_add_inner(y):
+        def curry_add_inner_2(z):
+            return x + y + z
+
+        return curry_add_inner_2
+
+    return curry_add_inner
+
+
+add_5 = curry_add(5)
+add_5_and_6 = add_5(6)
+print(add_5_and_6(7))
+
+print(curry_add(5)(6)(7))
